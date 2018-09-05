2a708d27a25e3d2c037d79e72c025a5ed2341386
Anthony <forzabarca94@gmail.com>
2018-09-05 16:12:42 +0200

    send work

---
+++ php/Login.php
@@ -0,0 +1,22 @@
+<?php 
+class Login extends Controller {
+		public static function VerifyLogin(){
+			$userLogin = $_POST['userLogin'];
+			$userPassword = sha1($_POST['userPassword']);
+			$queryLogin = Login::queryLogin($userLogin);
+			if (!empty($queryLogin)) {
+				if ($userPassword == $queryLogin['password']) {
+					echo "Good Password";
+					header("Location: dashboard");
+				} else {
+					echo "Wrong password!";
+					header("Location: index?status=wrongpassword");
+				}
+			} else {
+				echo "Can't found this user";
+				header("Location: index?status=usernotfound");
+			}
+		}
+	}
+
+ ?>
